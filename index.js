cf27985f223e450bd48cec11b1f1a38828b7e09b
Isa <izbagov0133@gmail.com>
2022-10-13 17:26:29 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const TelegramBot = require("node-telegram-bot-api");
+
+dotenv.config();
+
+const botToken = process.env.BOT_TOKEN;
+const webAppUrl = "https://golden-malabi-a16ad1.netlify.app";
+
+const bot = new TelegramBot(botToken, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }],
+        ],
+      },
+    });
+
+    await bot.sendMessage(
+      chatId,
+      "Заходи в наш интернет магазин по кнопке ниже",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
+          ],
+        },
+      }
+    );
+  }
+
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+
+      await bot.sendMessage(chatId, "Спасибо за обратную связь");
+      await bot.sendMessage(chatId, "Ваша страна: " + data?.country);
+      await bot.sendMessage(chatId, "Ваш улица: " + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
+      }, 3000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+});
+
+app.post("/web-data", async (req, res) => {
+  const { queryId, products, price } = req;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Успешная покупка",
+      input_message_content: {
+        message_text:
+          "Поздравлем с покупкой, вы преобрели товар на сумму " + price,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    console.log(e);
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Не удалось приобрести товар",
+      input_message_content: { message_text: "Не удалось приобрести товар" },
+    });
+    return res.status(500).json({});
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
